9b7c52f46a4fef59068f4e29af387cc9d9967b81
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-08-25 16:05:38 -0700

    feat: handle to display user-form with user-id from local-storage

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -3,7 +3,10 @@ import UserForm from './UserForm.jsx';
 import ChatRoom from './ChatRoom.jsx';
 
 export default function ChatBox() {
-  const [showForm, setShowForm] = useState(true);
+  // check if the user has already entered their information to show the chat room
+  const isShowForm = localStorage.getItem('userId');
+  // show the form if the user has not entered their information
+  const [showForm, setShowForm] = useState(!isShowForm);
 
   return (
     <div className='w-full'>
