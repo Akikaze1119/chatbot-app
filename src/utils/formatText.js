38c08bb2e80b346c405446081b306e7ad40d4e35
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-08-27 21:50:50 -0700

    fix: fix console error

--- src/utils/formatText.js
+++ src/utils/formatText.js
@@ -1,53 +1,46 @@
-import React from 'react';
-
+import { isValidElement } from 'react';
 export const formatText = (text) => {
   const lines = text.split('\n');
 
-  // Format the text into React elements
   const formattedLines = lines.map((line, index) => {
     if (line.startsWith('* **')) {
-      // Handle lines starting with * ** as bold text without wrapping in a list item
       const parts = line.split('**');
       return (
-        <p key={index}>
+        <span key={`bold-${index}`}>
           <strong>{parts[1]}</strong>
           {parts[2]}
-        </p>
+        </span>
       );
     } else if (line.startsWith('* ')) {
-      // Handle regular list items
-      return <li key={index}>{line.slice(2)}</li>;
+      return <li key={`li-${index}`}>{line.slice(2)}</li>;
     } else if (line.startsWith('**') && line.endsWith('**')) {
-      return <h3 key={index}>{line.slice(2, -2)}</h3>;
+      return <h3 key={`h3-${index}`}>{line.slice(2, -2)}</h3>;
     } else if (line.trim() === '') {
-      return <br key={index} />;
+      return <br key={`br-${index}`} />;
     } else {
-      return <p key={index}>{line}</p>;
+      return <p key={`p-${index}`}>{line}</p>;
     }
   });
 
-  // Group list items into unordered lists
   const formattedContent = [];
   let currentList = [];
 
-  // Loop through the formatted lines and group list items into unordered lists
   for (let i = 0; i < formattedLines.length; i++) {
     const element = formattedLines[i];
-    if (React.isValidElement(element) && element.type === 'li') {
+    if (isValidElement(element) && element.type === 'li') {
       currentList.push(element);
     } else {
       if (currentList.length > 0) {
-        formattedContent.push(<ul key={formattedContent.length}>{currentList}</ul>);
+        formattedContent.push(<ul key={`ul-${i}`}>{currentList}</ul>);
         currentList = [];
       }
       formattedContent.push(element);
     }
   }
 
-  // Add the last list if there are any list items left
   if (currentList.length > 0) {
-    formattedContent.push(<ul key={formattedContent.length}>{currentList}</ul>);
+    formattedContent.push(<ul key={`ul-last`}>{currentList}</ul>);
   }
 
-  return <div>{formattedContent}</div>;
+  return <>{formattedContent}</>;
 };
