03a15d90a289737a2ae92e474fe1a03a0187d159
Satomi Nagano / Akikaze1119 <132441773+Akikaze1119@users.noreply.github.com>
2024-08-27 17:57:30 -0700

    feat: add style to user-form

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,19 +1,36 @@
 import { useState } from 'react';
 import UserForm from './UserForm.jsx';
 import ChatRoom from './ChatRoom.jsx';
+import { ChevronDown, BotMessageSquare } from 'lucide-react';
 
-export default function ChatBox() {
+export default function ChatBox({ setOpen }) {
   // check if the user has already entered their information to show the chat room
   const isShowForm = localStorage.getItem('userId');
   // show the form if the user has not entered their information
   const [showForm, setShowForm] = useState(!isShowForm);
 
   return (
-    <div className='w-full'>
+    <>
+      <header className={'bg-violet-600 h-14 flex justify-between items-center'}>
+        <div className='flex gap-2 pl-2'>
+          <BotMessageSquare
+            className={'text-white rounded-full bg-violet-500 p-1 w-8 h-8 border-2 border-white'}
+          />
+          <h2 className='text-2xl font-bold text-white'>Support chat</h2>
+        </div>
+        <button
+          onClick={() => {
+            setOpen(false);
+          }}
+          className={`text-slate-800 p-2`}
+        >
+          <ChevronDown className={'text-white w-8 h-8'} />
+        </button>
+      </header>
       <section className='m-4 max-w-4xl'>
         {showForm && <UserForm onShowForm={setShowForm} />}
         {!showForm && <ChatRoom />}
       </section>
-    </div>
+    </>
   );
 }
--- src/components/ChatButton.jsx
+++ src/components/ChatButton.jsx
@@ -2,13 +2,17 @@ import { BotMessageSquare } from 'lucide-react';
 
 export default function ChatButton({ open, setOpen }) {
   return (
-    <button
-      className={
-        'w-14 h-14 rounded-full bg-indigo-800 flex justify-center items-center fixed bottom-4 right-4 md:bottom-10 md:right-20'
-      }
-      onClick={() => setOpen(!open)}
-    >
-      <BotMessageSquare className={'text-white'} />
-    </button>
+    <>
+      {!open && (
+        <button
+          className={
+            'w-14 h-14 rounded-full bg-violet-700 flex justify-center items-center fixed bottom-4 right-4 sm:bottom-6 sm:right-6'
+          }
+          onClick={() => setOpen(true)}
+        >
+          <BotMessageSquare className={'text-white'} />
+        </button>
+      )}
+    </>
   );
 }
